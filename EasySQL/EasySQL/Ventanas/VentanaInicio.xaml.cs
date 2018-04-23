e9cfd8612adf9a17ef8adc57fead422674ad5d74
alejandro <alejandro.rio@outlook.es>
2018-04-23 21:09:23 +0200

    Creada parte visual ventana registro. Organizado en carpetas

--- EasySQL/EasySQL/Ventanas/VentanaInicio.xaml.cs
+++ EasySQL/EasySQL/Ventanas/VentanaInicio.xaml.cs
@@ -10,19 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace EasySQL
+namespace EasySQL.Ventanas
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for VentanaInicio.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class VentanaInicio : Window
     {
-        public MainWindow()
+        public VentanaInicio()
         {
             InitializeComponent();
+        }
+
+        private void btnRegistro_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaRegistro vr = new VentanaRegistro();
+            vr.ShowDialog();
         }
     }
 }
