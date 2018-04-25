057b806b241390e0e1c8bb119431869d9f8b5b7e
alejandro <alejandro@kailneg>
2018-04-25 12:11:04 +0200

    Comienzo creacion clases manejadoras de bbdd

---
+++ EasySQL/EasySQL/BBDD/BBDDPrograma.cs
@@ -0,0 +1,24 @@
+﻿using EasySQL.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySQL.BBDD
+{
+    public abstract class BBDDPrograma
+    {
+        private static BBDDPrograma instancia;
+        public abstract List<Conexion> ObtenerConexiones(Usuario usuario);
+
+        public static BBDDPrograma ObtenerInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new BBDDProgramaImpl();
+            }
+            return instancia;
+        }
+    }
+}
---
+++ EasySQL/EasySQL/BBDD/BBDDProgramaImpl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySQL.Modelos;
+
+namespace EasySQL.BBDD
+{
+    public class BBDDProgramaImpl : BBDDPrograma
+    {
+        public override List<Conexion> ObtenerConexiones(Usuario usuario)
+        {
+            return null;
+            //throw new NotImplementedException();
+        }
+    }
+}
