44b7572d825b501796409878483608b46d97a7ca
alejandro <alejandro@kailneg>
2018-04-25 12:11:29 +0200

    Comienzo ventana Conexion

---
+++ EasySQL/EasySQL/Ventanas/VentanaConexion.xaml.cs
@@ -0,0 +1,36 @@
+﻿using EasySQL.BBDD;
+using EasySQL.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasySQL.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaConexion.xaml
+    /// </summary>
+    public partial class VentanaConexion : Window
+    {
+        private Usuario usuarioActivo;
+        private BBDDPrograma datosPrograma;
+
+        public VentanaConexion(Usuario usuario)
+        {
+            InitializeComponent();
+            if (usuario != null)
+            {
+                datosPrograma.ObtenerConexiones(usuarioActivo);
+            }
+        }
+    }
+}
