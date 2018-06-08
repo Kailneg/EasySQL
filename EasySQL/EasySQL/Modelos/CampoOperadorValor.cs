a8f7f5377146ae56a317dc9cb5a3bd1f3e365ab9
alejandro <alejandro@kailneg>
2018-06-08 08:26:26 +0200

    Modelos CampoValor y CampoOperadorValor creados.

---
+++ EasySQL/EasySQL/Modelos/CampoOperadorValor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySQL.Modelos
+{
+    public class CampoOperadorValor
+    {
+        public string Campo { get; set; }
+        public string Operador { get; set; }
+        public string Valor { get; set; }
+
+        public CampoOperadorValor(string campo, string operador, string valor)
+        {
+            Campo = campo;
+            Operador = operador;
+            Valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return Campo + " " + Operador + " " + Valor;
+        }
+    }
+}
---
+++ EasySQL/EasySQL/Modelos/CampoValor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySQL.Modelos
+{
+    public class CampoValor
+    {
+        public string Campo { get; set; }
+        public string Valor { get; set; }
+
+        public CampoValor(string campo, string valor)
+        {
+            Campo = campo;
+            Valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return Campo + ": " + Valor;
+        }
+    }
+}
