1b043be599383d8e7f2a7e55f2164f85c6cd9cce
alejandro <alejandro@kailneg>
2018-06-14 08:48:34 +0200

    VCreateTable ahora permite añadir una o múltiples Primary Keys a los campos.

--- EasySQL/EasySQL/Ventanas/Operaciones/VCreateTable.xaml.cs
+++ EasySQL/EasySQL/Ventanas/Operaciones/VCreateTable.xaml.cs
@@ -54,10 +54,11 @@ namespace EasySQL.Ventanas.Operaciones
             // Mirar cada uno de los campos del formulario para escribir el comando SQL resultante.
             // Comando original: CREATE TABLE 
             string datos = textoComandoOriginal;
+            List<string> primaryKeys = new List<string>();
 
             // Añado nombre tabla -> CREATE TABLE nombre (
-            datos += Comprueba.EliminarResto(txtbox.Text);
-            datos += "(";
+            datos += Comprueba.EliminarResto(txtTabla.Text);
+            datos += " (";
 
             // Añadir cada uno de los valores de los textbox, separado por coma junto al tipo de dato
             // -> CREATE TABLE nombre (columna1 tipodato1, columna2 tipodato2
@@ -65,8 +66,12 @@ namespace EasySQL.Ventanas.Operaciones
             for (int i = 0; i < numColumnas; i++)
             {
                 // Obtiene cada TextBox y ComboBox de cada fila
+                CheckBox chkPk = stackCheckBoxes.Children[i] as CheckBox;
                 TextBox txtColumna = stackTextBoxes.Children[i] as TextBox;
                 ComboBox cmbTipoDato = stackComboBoxes.Children[i] as ComboBox;
+
+                if (chkPk.IsChecked.Value)
+                    primaryKeys.Add(txtColumna.Text);
 
                 datos += Comprueba.EliminarResto(txtColumna.Text);
                 datos += " ";
@@ -75,9 +80,22 @@ namespace EasySQL.Ventanas.Operaciones
                 datos += ", ";
             }
 
+
+            // -> CREATE TABLE nombre (columna1 tipodato1, columna2 tipodato2)
+            // += CONSTRAINT PK_<nombreTabla> PRIMARY KEY (columna2marcado1, columna2marcada2)
+            if (primaryKeys.Count > 0)
+            {
+                datos += " CONSTRAINT PK_" + txtTabla.Text + " PRIMARY KEY (";
+                foreach (string pk in primaryKeys)
+                {
+                    datos += pk + ", ";
+                }
+                datos += ")";
+            }
+
             // Elimina última coma y pone paréntesis de cierre
             if (datos.Contains(','))
-                datos = datos.Substring(0, datos.LastIndexOf(','));
+                datos = datos.Remove(datos.LastIndexOf(','), 1);
             datos += ")";
 
             // Asigna datos a label y al comando
@@ -94,6 +112,17 @@ namespace EasySQL.Ventanas.Operaciones
             {
                 numColumnas = Int32.Parse(cmbCampos.SelectedItem.ToString());
                 string[] tiposDatos = Comando.TiposDatos(conexionActual);
+
+                stackCheckBoxes.Children.Clear();
+                for (int i = 0; i < numColumnas; i++)
+                {
+                    CheckBox check = new CheckBox();
+                    check.Height = 25;
+                    check.Margin = new Thickness(0, 5, 0, 5);
+                    check.VerticalContentAlignment = VerticalAlignment.Center;
+                    check.HorizontalAlignment = HorizontalAlignment.Center;
+                    stackCheckBoxes.Children.Add(check);
+                }
 
                 stackTextBoxes.Children.Clear();
                 for (int i = 0; i < numColumnas; i++)
@@ -125,7 +154,7 @@ namespace EasySQL.Ventanas.Operaciones
             int resultado = Operacion.ExecuteNonQuery(conexionActual, comandoEnviar);
             if (resultado == -1)
             {
-                MessageBox.Show("Tabla \"" + Comprueba.EliminarResto(txtbox.Text) + "\" en base de datos " +
+                MessageBox.Show("Tabla \"" + Comprueba.EliminarResto(txtTabla.Text) + "\" en base de datos " +
                     "\"" + conexionActual.BaseDatos +  "\" creada con éxito.");
             }
         }
